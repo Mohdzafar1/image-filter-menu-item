d706dff5e46c52411b6990cfdd1ae726705e532c
Mohd zafar <68924688+Mohdzafar1@users.noreply.github.com>
2022-08-07 14:08:59 +0530

    image filter menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AllData from "./Components/Pages/AllData"
 
 function App() {
+  const[images,setImages]=useState(AllData)
+  
+   const allItem=()=>{
+    const finalData=AllData.filter((value)=>{
+      return value;
+    })
+    setImages(finalData)
+}
+const onlyPython=(categoryItem)=>{
+  const finalData=AllData.filter((value)=>{
+    return  value.category ==categoryItem
+  })
+setImages(finalData)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+     <div className="container">
+      <div className="row pt-3">
+        <div className="col-lg-3 col text-center">
+         <button className='btn btn-outline-dark shadow-none'onClick={allItem}>ALl</button>
+        </div>
+        <div className="col-lg-3 col text-center">
+         <button className='btn btn-outline-primary'onClick={()=>onlyPython("python")}>PYTHON</button>
+        </div>
+        <div className="col-lg-3 col text-center">
+         <button className='btn btn-outline-primary'onClick={()=>onlyPython("office")}>OFFICE</button>
+        </div>
+        <div className="col-lg-3 col text-center">
+         <button className='btn btn-outline-primary'onClick={()=>onlyPython("React")}>REACT.JS</button>
+        </div>
+      </div>
+     </div>
+   
+
+    <div className="container">
+       <div className="row row-cols-1 row-cols-sm-2 row-md-3">
+         {
+          images.map((value)=>{
+            return(
+              <>
+              <div className="col-lg-3 pt-3 pb-3">
+              <img src={value.image} className="img-fluid"/>
+              </div>
+            </>
+            )
+          })
+         }
+       </div>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Pages/AllData.js
@@ -0,0 +1,69 @@
+import office1 from "../Images/office1.jpg"
+import office2 from "../Images/office2.jpg"
+import office3 from "../Images/office3.jpg"
+import Python1 from "../Images/python1.jpeg"
+import Python2 from "../Images/python2.jpeg"
+import Python3 from "../Images/python3.jpeg"
+import React1 from "../Images/react1.webp"
+import React2 from "../Images/react2.webp"
+import React3 from "../Images/react3.webp"
+import React4 from "../Images/react4.webp"
+
+const Data=[
+    {
+        id:1,
+        image:office1,
+        category:"office"
+    },
+    {
+        id:2,
+        image:office2,
+        category:"office"
+    },
+    {
+        id:3,
+        image:office3,
+        category:"office"
+    },
+    {
+        id:4,
+        image:Python1,
+        category:"python"
+    },
+    {
+        id:5,
+        image:Python2,
+        category:"python"
+    },
+    {
+        id:6,
+        image:Python3,
+        category:"python"
+    },
+    {
+        id:7,
+        image:React1,
+        category:"React"
+    },
+    {
+        id:8,
+        image:React2,
+        category:"React"
+    },
+    {
+        id:9,
+        image:React3,
+        category:"React"
+    },
+    {
+        id:10,
+        image:React4,
+        category:"React"
+    },
+]
+
+export default Data;
+
+
+
+
